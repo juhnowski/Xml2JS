523bd90fd50587939adb956bb7aadeed29c56a8a
Juhnowski <juhnowski@gmail.com>
2017-05-19 23:27:14 +0300

    agadaNkAra offline work

--- src/xml2js/Xml2JS.java
+++ src/xml2js/Xml2JS.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xml2js;
 
 import java.io.BufferedWriter;
@@ -17,9 +12,10 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
- * @author ilya
+ * @author Ilya Juhnowski
  */
 public class Xml2JS {
 
@@ -28,7 +24,7 @@ public class Xml2JS {
      */
     public static void main(String[] args) {
 
-        File f = new File("wil.js");
+        File f = new File("wil.html");
         try {
             f.createNewFile();
         } catch (IOException ex) {
@@ -38,40 +34,66 @@ public class Xml2JS {
         try (
                 FileInputStream inputStream = new FileInputStream("wil.xml");
                 Scanner sc = new Scanner(inputStream, "UTF-8");
-                OutputStream fos = new FileOutputStream("wil.js");
+                OutputStream fos = new FileOutputStream("wil.html");
                 OutputStreamWriter osr = new OutputStreamWriter(fos, Charset.forName("UTF-8"));
                 BufferedWriter bw = new BufferedWriter(osr);) {
             StringBuilder sb = new StringBuilder();
             String class_wrapper
-                    = "class FreqWordList { "
-                    + " constructor() {"
-                    + "   this.version = 1;"
-                    + " }";
+                    = "<!DOCTYPE HTML><html><head><script type=\"text/javascript\">\n"
+                    + "         var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);\n"
+                    + "         var msg;\n"
+                    + "         db.transaction(function (tx) {tx.executeSql('DROP TABLE DICT');});"
+                    + "         db.transaction(function (tx) {\n"
+                    + "            tx.executeSql('CREATE TABLE IF NOT EXISTS DICT (key unique, val)');});";
             sb.append(class_wrapper);
-            sb.append("fill() {");
 
             int i = 0;
             while (sc.hasNextLine()) {
 
                 String line = sc.nextLine();
-                i++;
 
-                String[] sarray = line.split(",");
-                String word = sarray[0];
-                if (word.length() > 0) {
-                    sb.append("localStorage.setItem(\"");
-                    sb.append(sarray[0]);
-                    sb.append("\", \"");
-                    int stringTokenizer = new StringTokenizer(" " + line + " ", ";").countTokens() - 1;
-                    sb.append(stringTokenizer);
-                    sb.append("\");");
-                    bw.append(sb.toString());
-                    bw.newLine();
-                    bw.flush();
-                    sb = new StringBuilder();
+                if (line.contains("<H1>")) {
+                    i++;
+                    String[] sarray = line.split("</key2></h><body>");
+                    String[] warray = sarray[0].split("</key1><key2>");
+                    
+                    String word = warray[1].replaceAll("'", "&acute;");;
+                    if (word.length() > 0) {
+                        String[] s0 = line.split("<body>.");
+                        String[] s1 = s0[1].split("</body>");
+                        String s2 = s1[0].replaceAll("'", "&acute;");
+                        String s3 = s2.replaceAll("<s>", "<b>");
+                        String body = s3.replaceAll("</s>", "</b>");
+
+                        sb.append("db.transaction(function (tx) {\n");
+                        sb.append("tx.executeSql('INSERT INTO DICT (key, val) VALUES (\""+word+"\", \""+body+"\")');});");
+                        bw.append(sb.toString());
+                        bw.newLine();
+                        bw.flush();
+                        sb = new StringBuilder();
+                    }
+
                 }
             }
-            sb.append("}}");
+            sb.append("\n"
+                    + "         db.transaction(function (tx) {\n"
+                    + "            tx.executeSql('SELECT * FROM DICT', [], function (tx, results) {\n"
+                    + "               var len = results.rows.length, i;\n"
+                    + "               msg = \"<p>Found rows: \" + len + \"</p>\";\n"
+                    + "               document.querySelector('#status').innerHTML +=  msg;\n"
+                    + "\n"
+                    + "               for (i = 0; i < len; i++){\n"
+                    + "                  msg = \"<p><b>\" + results.rows.item(i).key + \"</b></p><br>\" + results.rows.item(i).val + \"<br>\";\n"
+                    + "                  document.querySelector('#status').innerHTML +=  msg;\n"
+                    + "               }\n"
+                    + "            }, null);\n"
+                    + "         });\n"
+                    + "      </script>\n"
+                    + "   </head>\n"
+                    + "   <body>\n"
+                    + "      <div id=\"status\" name=\"status\">Status Message</div>\n"
+                    + "   </body>\n"
+                    + "</html>");
             bw.append(sb.toString());
             bw.newLine();
             osr.flush();
@@ -90,5 +112,4 @@ public class Xml2JS {
 
     }
 
-    
 }
